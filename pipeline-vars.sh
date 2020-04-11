458752823af3fe4185165cfabfc27db2553119b6
namannagar <naman.nagar4@gmail.com>
2020-04-12 03:35:39 +0530

    Update pipeline-vars.sh

--- pipeline-vars.sh
+++ pipeline-vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CMD_BUILD="build.py"
-export CMD_BUILDv1="buildv1.py"
+export CMD_BUILDV1="buildv1.py"
 export CMD_DIFF="diff.py"
 export CMD_INSTALL="helm-install.py"
 export VALUES_DIR="values"
