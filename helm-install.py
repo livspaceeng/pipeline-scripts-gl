caf550a78ec751a12ef062095eff124d12fad4ae
Naman Nagar <naman.nagar@livspace.com>
2020-04-11 20:51:07 +0530

    updated file

--- helm-install.py
+++ helm-install.py
@@ -85,24 +85,24 @@ def beforeScript(repo):
     before_script= "apk update and apk add git"
     before_script8= "apk --no-cache add git"
     before_script9= "git submodule update --recursive --remote"
-    # before_script1 = 'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
-    # before_script2 = "eval $(ssh-agent -s)"
-    # before_script3 = """echo "$SSH_PRIIVATE_KEY2" | tr -d '\r' | ssh-add -"""
-    # before_script4 = "mkdir -p ~/.ssh"
-    # before_script5 = "chmod 700 ~/.ssh"
-    # before_script6 = """echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts"""
-    # before_script7 = "chmod 644 ~/.ssh/known_hosts"
+    before_script1 = 'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
+    before_script2 = "eval $(ssh-agent -s)"
+    before_script3 = """echo "$SSH_PRIIVATE_KEY2" | tr -d '\r' | ssh-add -"""
+    before_script4 = "mkdir -p ~/.ssh"
+    before_script5 = "chmod 700 ~/.ssh"
+    before_script6 = """echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts"""
+    before_script7 = "chmod 644 ~/.ssh/known_hosts"
     script.append(before_script)
     script.append(before_script8)
     script.append(before_script9)
     
-    # script.append(before_script1)
-    # script.append(before_script2)
-    # script.append(before_script3)
-    # script.append(before_script4)
-    # script.append(before_script5)
-    # script.append(before_script6)
-    # script.append(before_script7)
+    script.append(before_script1)
+    script.append(before_script2)
+    script.append(before_script3)
+    script.append(before_script4)
+    script.append(before_script5)
+    script.append(before_script6)
+    script.append(before_script7)
     script.append("helm init -c --tiller-namespace $TILLER_NAMESPACE")
     for k,rep in repo.items():
         script.append("helm repo add " + rep['label'] + " " + rep['url'])
